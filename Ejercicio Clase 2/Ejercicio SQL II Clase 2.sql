85b0e555797c40fb8b68f7f67c7b093ce8819f7b
Oscar <oscarruina@hotmail.com>
2021-12-06 15:48:47 -0300

    Solved exercise 2

---
+++ Ejercicio Clase 2/Ejercicio SQL II Clase 2.sql
@@ -0,0 +1,17 @@
+#1
+select e.nombre,e.apellido, c.nombre from estudiante as e inner join curso as c;
+
+#2
+select e.nombre,e.apellido, c.nombre from estudiante as e inner join curso as c order by c.nombre;
+
+#3
+select p.nombre,p.apellido,c.nombre from profesor as p inner join curso as c;
+
+#4
+select p.nombre,p.apellido,c.nombre from profesor as p inner join curso as c order by c.nombre;
+
+#5
+select c.codigo, c.nombre, c.cupo - count(i.ESTUDIANTE_legajo) as cupos_disponibles from curso as c left join inscripcion as i  on c.codigo = i.CURSO_codigo group by c.nombre;
+
+#6 no sale 
+ 
