6aae416737de5879554ff08ff21aea8c40232c88
Oscar <oscarruina@hotmail.com>
2021-12-06 18:55:07 -0300

    Fixed some issues

--- Ejercicio Clase 2/Ejercicio SQL II Clase 2.sql
+++ Ejercicio Clase 2/Ejercicio SQL II Clase 2.sql
@@ -13,5 +13,7 @@ select p.nombre,p.apellido,c.nombre from profesor as p inner join curso as c ord
 #5
 select c.codigo, c.nombre, c.cupo - count(i.ESTUDIANTE_legajo) as cupos_disponibles from curso as c left join inscripcion as i  on c.codigo = i.CURSO_codigo group by c.nombre;
 
-#6 no sale 
+#6
+select c.codigo, c.nombre, c.cupo - count(i.ESTUDIANTE_legajo) as cupos_disponibles from curso as c left join inscripcion as i  on c.codigo = i.CURSO_codigo group by c.nombre having cupos_disponibles < 10;
+
  
